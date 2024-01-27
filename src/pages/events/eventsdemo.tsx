a4df8807e27c9ecf1f1f65923d22b91f86dc1b55
[Arvind B S] <arvind_b6u93@tataneuskills.com>
2024-01-27 13:51:38 +0530

    TypeScript

--- src/pages/events/eventsdemo.tsx
+++ src/pages/events/eventsdemo.tsx
@@ -15,7 +15,7 @@ function EventPage({events}:any) {
     return ( <>
         <h1>Events</h1>
         {
-            events.map((event)=>(
+            events.map((event:any)=>(
                 <div key={event.id}>
                     <h2>{event.title}</h2>
                     <p>Date: {event.date}</p>
